a2d12a061058eb13ed61b98b77878650a147f6a6
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-21 23:26:00 +0100

    add logic for view class in the view package

---
+++ src/main/java/com/gloriane/view/ContactView.java
@@ -0,0 +1,39 @@
+package com.gloriane.view;
+
+import com.gloriane.model.Contact;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ContactView {
+    Scanner scanner = new Scanner(System.in);
+
+    public String getUserInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public void displaymenu() {
+        System.out.println("\n Contact App Menu");
+        System.out.println("1. Display All Contact");
+        System.out.println("2. Add New Contact");
+        System.out.println("3. Exit");
+    }
+
+    public void displayContacts(List<Contact> contacts) {
+        if (contacts == null || contacts.isEmpty()) {
+            System.out.println("No contacts found.");
+        } else {
+            System.out.println("\nContact List:");
+            contacts.forEach(System.out::println);
+        }
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayError(String message) {
+        System.out.println("Error: " + message);
+    }
+}
