2db2ee8fbb7d07824fcbc152cb6d82756a5b6b85
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-21 22:46:53 +0100

    add the contact storage exception and duplicate contact exception in the exception package

---
+++ src/main/java/com/gloriane/exception/DuplicateContactException.java
@@ -0,0 +1,7 @@
+package com.gloriane.exception;
+
+public class DuplicateContactException extends Exception {
+    public DuplicateContactException(String message) {
+        super(message);
+    }
+}
